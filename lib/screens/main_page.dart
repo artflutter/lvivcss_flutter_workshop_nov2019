d430fcf03f7198692e7f203caa1dbbc38a0b987d
vasilich <vasilich@grafin.kiev.ua>
2019-10-24 15:23:37 +0300

    Tab navigation

---
+++ lib/screens/main_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Container(),
+    Container(),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google translate'),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.g_translate),
+            title: Text('Translate'),
+          ),
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favorite'),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:lvivcss_flutter_workshop_nov2019/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(GoogleTranslateApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
