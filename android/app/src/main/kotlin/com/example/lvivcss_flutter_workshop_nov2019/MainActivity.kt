03a75ec6e50683d9b9452574a4bec0400043a785
vasilich <vasilich@grafin.kiev.ua>
2019-10-24 12:42:36 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/lvivcss_flutter_workshop_nov2019/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lvivcss_flutter_workshop_nov2019
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
